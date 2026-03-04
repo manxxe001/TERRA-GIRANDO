0e82fea001802f24c116f8e00cf01732d8a184f0
manx01n02 <147075168+manxxe001@users.noreply.github.com>
2026-03-04 15:40:06 -0300

    esfera

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+    createCanvas(1920,1080,WEBGL);//plano 3D
+    terra=loadImage('terrajpg')
+}
+
+function draw(){
+    background(205,102,94);// cor de fundo
+    rotateY(millis()/1000);//rotação da terra
+    TextTrackCue(terra);// textura da terra, imagem.
+    SpeechRecognitionAlternative(80,100);//esfera 3D
+}
